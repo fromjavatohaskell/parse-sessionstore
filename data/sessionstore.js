b141e5634da49b73c6cf669e8992554039e0b335
FromJavaToHaskell <fromjavatohaskell@gmail.com>
2018-11-15 19:43:25 -0500

    add example sessionstore.js file

---
+++ data/sessionstore.js
@@ -0,0 +1,52 @@
+{
+  "version": [
+    "sessionrestore",
+    1
+  ],
+  "windows": [
+    {
+      "tabs": [
+        {
+          "entries": [
+            {
+              "url": "about:home",
+              "title": "New Tab"
+            }
+          ]
+        },
+        {
+          "entries": [
+            {
+              "url": "https://duckduckgo.com/",
+              "title": "DuckDuckGo — Privacy, simplified."
+            }
+          ]
+        },
+        {
+          "entries": [
+            {
+              "url": "https://www.youtube.com/",
+              "title": "YouTube"
+            }
+          ]
+        },
+        {
+          "entries": [
+            {
+              "url": "https://www.reddit.com/",
+              "title": "reddit: the front page of the internet"
+            }
+          ]
+        },
+        {
+          "entries": [
+            {
+              "url": "https://github.com/",
+              "title": "The world’s leading software development platform · GitHub"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
