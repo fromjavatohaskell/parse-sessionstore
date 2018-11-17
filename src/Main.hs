17398e7b55f55d4dd7bd6a3d65cbe4f0ebd80ed9
FromJavaToHaskell <fromjavatohaskell@gmail.com>
2018-11-17 13:35:19 -0500

    initial version of parse-sesionstore

---
+++ src/Main.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- license MIT https://raw.githubusercontent.com/fromjavatohaskell/parse-sessionstore/master/LICENSE
+
+module Main where
+
+import           Data.Aeson
+import           Data.Aeson.Types
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.Text            as T
+import qualified Data.Text.IO         as T
+import qualified System.IO            as IO
+
+getUrlAndTitle :: Value -> Parser [T.Text]
+getUrlAndTitle = withObject "urlAndTitle" $ \o -> do
+  url   <- o .: "url"
+  title <- o .: "title"
+  return $ [url <> " => " <> title]
+
+entries :: (Value -> Parser [a]) -> Value -> Parser [a]
+entries f = withArray "entries" $ \arr -> concat <$> traverse f arr
+
+field :: FromJSON a => T.Text -> (a -> Parser b) -> Value -> Parser b
+field key f = withObject (T.unpack key) $ \o -> (o .: key) >>= f
+
+extractData :: Value -> Parser [T.Text]
+extractData = f "windows" $ e $ f "tabs" $ e $ f "entries" $ e $ getUrlAndTitle
+ where f = field; e = entries
+
+-- LB.hGetContents IO.stdin :: IO LB.ByteString
+-- decode :: LB.ByteString -> Maybe Value
+-- parseMaybe extractData :: Value -> Maybe [T.Text]
+-- T.putStrLn :: T.Text -> IO ()
+-- main :: IO [()]
+
+main = do
+  contents <- LB.hGetContents IO.stdin
+  case decode contents >>= parseMaybe extractData of
+    Just list -> traverse T.putStrLn list
+    Nothing ->   traverse (T.hPutStrLn IO.stderr) [T.pack "error parsing session store"]
+
